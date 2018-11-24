05fc6e40c968714916adcd05896bd6876da24f17
manumohan009 <manumohan.cvav@gmail.com>
2018-11-25 00:05:46 +0530

    71. Implementing Settings (Incomplete)  & 73. Deploy To Firebase

--- src/components/layout/AppNavbar.js
+++ src/components/layout/AppNavbar.js
@@ -75,6 +75,21 @@ class AppNavbar extends Component {
                 </li>
               </ul>
             ) : null}
+
+            <ul className='navbar-nav ml-auto'>
+              <li className='nav-item'>
+                <Link to='/login' className='nav-link'>
+                  {' '}
+                  Login
+                </Link>
+              </li>
+              <li className='nav-item'>
+                <Link to='/register' className='nav-link'>
+                  {' '}
+                  Register
+                </Link>
+              </li>
+            </ul>
           </div>
         </div>
       </nav>
